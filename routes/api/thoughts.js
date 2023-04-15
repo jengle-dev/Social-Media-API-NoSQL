d5ba1e5b6bbb0e1b61cacc67444748ef4181ddaa
jengle-dev <j.engle.dev@gmail.com>
2023-04-15 10:43:41 -0500

    added pseudo-code in routes, tried to set up basic controllers and models

---
+++ routes/api/thoughts.js
@@ -0,0 +1,18 @@
+//CRUD - Create, Read , Update, Delete
+// formatted JSON
+// if user status is author allow update/delete, else don't
+
+// GET all thoughts
+// CREATE a new thought
+// UPDATE-POST an existing thought
+
+// CREATE a new reaction to another user's thought
+// CREATE a new reaction to same user's thought
+
+// UPDATE-POST an existing reaction to a thought
+
+// GET specific thought to DELETE a specific reaction
+// GET specific thought to DELETE all reactions
+
+// DELETE specific thought
+// DELETE all thoughts
---
+++ routes/api/users.js
@@ -0,0 +1,23 @@
+//CRUD - Create, Read , Update, Delete
+// formatted JSON
+// if user status is author allow update/delete, else don't
+
+
+// GET all users
+// GET all users & all thoughts/posts
+// GET all users & all thoughts/posts and filter by Date
+
+// GET specific user
+// GET specific users & all thoughts/posts
+// GET specific users & specific thought/post
+// GET specific users & specific thought/post and update
+// GET specific users & specific thought/post and delete
+
+// CREATE new user
+// CREATE new thought
+
+
+// GET specific user and select a thought to POST a reaction
+
+// DELETE specific user & all thoughts & all reactions they have posted and all reactions posted to their thoughts
+
